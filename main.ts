cbb92fc119185d9afb3d92d11c8d0e7d98a72b55
Jan Melcher <jan.melcher@aeb.com>
2025-02-27 19:50:36 +0100

    feat: include removals and make everything more realistic
    Every step now inserts some documents and then removes a random span of documents (NOT sorted by
    age).
    
    Also, add a category field for query tests

--- main.ts
+++ main.ts
@@ -26,6 +26,7 @@ async function setup() {
         await collection.drop();
     }
     await collection.create();
+    await collection.ensureIndex({ type: 'persistent', fields: ['gauge'] });
 
     const view = database.view(VIEW_NAME);
     if (await view.exists()) {
@@ -52,30 +53,71 @@ async function setup() {
                     field5: {
                         analyzers: ['identity'],
                     },
+                    category: {
+                        analyzers: ['identity'],
+                    },
                 },
             },
         },
         commitIntervalMsec: 1000,
         consolidationIntervalMsec: 1000,
+        consolidationPolicy: {
+            type: 'tier',
+            segmentsMax: 3_000_000,
+        },
+        /*consolidationPolicy: {
+            type: 'tier',
+            segmentsMin: 3,
+            segmentsBytesFloor: 500000
+        }*/
     });
 
+    await reInitData();
+}
+
+async function reInitData() {
+    await collection.truncate();
     console.log(`Inserting ${INITIAL_COUNT} initial documents...`);
-    await insert(1_000_000);
+    for (let i = 0; i < 1000; i++) {
+        await insert(1000);
+        await setTimeout(10);
+    }
     console.log('Done.');
 }
 
 async function insert(count = 1) {
     await database.query(aql`
         FOR index in RANGE(1, ${count})
-        INSERT { field1: RANDOM_TOKEN(60), field2: RANDOM_TOKEN(60), field3: RANDOM_TOKEN(60), field4: RANDOM_TOKEN(60), field5: RANDOM_TOKEN(60) } IN ${collection}
+        INSERT { field1: RANDOM_TOKEN(60), field2: RANDOM_TOKEN(60), field3: RANDOM_TOKEN(60), field4: RANDOM_TOKEN(60), field5: RANDOM_TOKEN(60), category: FLOOR(RAND() * 8), gauge: RAND() } IN ${collection}
+    `);
+}
+
+async function remove(count = 1) {
+    // remove random slice of documents
+    // the "gauge" field is evenly distributed between 0 and 1
+    // there is a regular (persistent) index on gauge, so we can cheaply do this
+    const targetGauge = Math.random();
+    await database.query(aql`
+        FOR doc IN ${collection} FILTER doc.gauge > ${targetGauge} SORT doc.gauge ASC LIMIT 0, ${count} REMOVE doc IN ${collection}
     `);
 }
 
 async function run() {
+    let totalInserts = 0;
+    let totalRemovals = 0;
     while (true) {
-        await insert(1);
-        const delay = Math.random() * 5000;
-        console.log('Inserted one document');
+        const inserts = Math.round(10 ** (Math.random() * 2));
+        const removals = Math.round(10 ** (Math.random() * 2));
+        const delay = Math.round(10 ** (Math.random() * 4));
+        await insert(inserts);
+        await remove(inserts);
+        totalInserts += inserts;
+        totalRemovals += removals;
+        console.log(
+            `Inserted ${String(inserts).padStart(4)} documents, removed ${String(removals).padStart(
+                4,
+            )} docs (${totalInserts} inserts in total, ${totalRemovals} removals in total), waiting ${delay} ms`,
+        );
         await setTimeout(delay);
     }
 }
